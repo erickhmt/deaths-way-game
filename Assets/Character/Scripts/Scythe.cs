b190d38347bbb14367fb5920c003b24ecdde6ee9
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2022-04-02 05:01:11 -0300

    Fix throw object

--- Assets/Character/Scripts/Scythe.cs
+++ Assets/Character/Scripts/Scythe.cs
@@ -25,11 +25,12 @@ public class Scythe : MonoBehaviour
     {
         if(!playerTransform)
         {
-            spriteTransform.Rotate(new Vector3(0f, 0f, -speed * Time.fixedDeltaTime));
+            spriteTransform.Rotate(new Vector3(0f, 0f, -speed * 30 * Time.fixedDeltaTime));
+            rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
         }
         else
         {
-            rb.velocity = Vector2.zero;
+            // rb.velocity = Vector2.zero;
             transform.SetParent(handBone);
             transform.localPosition = new Vector3(0f, 0f, 0f);
             transform.localRotation = new Quaternion(0f, 0f, 0f, 0f);
@@ -45,15 +46,15 @@ public class Scythe : MonoBehaviour
         }
         else
         {
-            Vector2 direction = -rb.velocity.normalized;
-            float randomDirectionValue = Random.Range(-0.75f, 0.75f);
+            Vector2 direction_temp = -direction;
+            float randomDirectionValue = Random.Range(-.5f, .5f);
             
-            if(direction.x > direction.y)
-                direction = new Vector2(direction.x, direction.y + randomDirectionValue).normalized;
-            else if(direction.x < direction.y)
-                direction = new Vector2(direction.x + randomDirectionValue, direction.y).normalized;
+            if(Mathf.Abs(direction_temp.x) > Mathf.Abs(direction_temp.y))
+                direction_temp = new Vector2(direction_temp.x, direction_temp.y + randomDirectionValue).normalized;
+            else if(Mathf.Abs(direction_temp.x) < Mathf.Abs(direction_temp.y))
+                direction_temp = new Vector2(direction_temp.x + randomDirectionValue, direction_temp.y).normalized;
 
-            rb.velocity = direction * speed * Time.fixedDeltaTime; 
+            direction = direction_temp; 
         }
     }
 
@@ -63,7 +64,7 @@ public class Scythe : MonoBehaviour
         {
             transform.SetParent(null);
             playerTransform = null;
-            rb.velocity = direction * speed * Time.fixedDeltaTime; 
+            this.direction = direction; 
         }
     }
 }
